471ac591b96864e4bd44181d5470036a2ed45bd5
kalle <kmthorsnes@gmail.com>
2018-12-04 21:50:40 +0100

    starting video 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-// let's go!
-alert('halla');
-console.log('jeg er kul');
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/storepicker';
+
+
+render(<StorePicker></StorePicker>, document.querySelector('#main'));
