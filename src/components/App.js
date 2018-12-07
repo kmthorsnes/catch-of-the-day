13de6ec970898e0bae094acb7ccd01fe645715b3
kalle <kmthorsnes@gmail.com>
2018-12-07 23:10:10 +0100

    finish with video #7

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import { truncate } from "fs";
 
 class App extends React.Component {
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="fresh seafood marked"/>
         </div>
         <Order />
         <Inventory />
