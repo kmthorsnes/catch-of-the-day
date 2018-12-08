fb0f9fe6a85437cbf16273cc8686b85125636b0d
Thorsnes <Karl-Magnus.Thorsnes@kantar.com>
2018-12-08 15:52:24 +0100

    finnish video #10

--- src/components/storepicker.js
+++ src/components/storepicker.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { format } from 'url';
+import { getFunName } from  '../helpers';
 
 class StorePicker extends React.Component {
     render() {
         return (
         <form className="store-selector">
             <h2>Please enter a store</h2>
-            <input type="text" required placeholder="Store Name" />
+            <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
             <button type="submit">Visit store -> </button>
         </form>
 
