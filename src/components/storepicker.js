17a18a583760333b8c6b20fa352207c3d82e6b28
Thorsnes <Karl-Magnus.Thorsnes@kantar.com>
2018-12-08 16:17:45 +0100

    finnish video #11

--- src/components/storepicker.js
+++ src/components/storepicker.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { format } from 'url';
-import { getFunName } from  '../helpers';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+   
+    goToStore = (event) =>  {
+        // Stop from submiting
+        event.preventDefault();
+        // Get text from that input
+        console.log(this);
+        // Change the page to store/whatever they entererd
+
+    }
     render() {
         return (
-        <form className="store-selector">
-            <h2>Please enter a store</h2>
-            <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
-            <button type="submit">Visit store -> </button>
-        </form>
+            <form className="store-selector" onSubmit={this.goToStore}>
+                <h2>Please enter a store</h2>
+                <input 
+                    type="text"
+                    ref={this.myInput} 
+                    required placeholder="Store Name" 
+                    defaultValue={getFunName()} />
+                <button type="submit">Visit store -> </button>
+            </form>
 
-        ) 
-        
+        )
+
     }
 }
+
+
 
 export default StorePicker; 
