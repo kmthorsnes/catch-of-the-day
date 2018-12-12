631aa5e1ac6bf519e3c79433ab1dc9e72543c2b8
Thorsnes <Karl-Magnus.Thorsnes@kantar.com>
2018-12-12 15:56:08 +0100

    video #15 7:35

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
+// run npm install --save-dev case-sensitive-paths-webpack-plugin 
+// if case trouble
+
 import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+
+
 
 class App extends React.Component {
   state = {
@@ -18,13 +24,15 @@ class App extends React.Component {
     this.setState({ fishes });
   };
   loadSampleFishes = () => {
-    this.setState({ fishes: sampleFishes});
+    this.setState({ fishes: sampleFishes });
   };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}          </ul>
         </div>
         <Order />
         <Inventory
